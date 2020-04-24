d62153ae8b03906b5b0a628c96cf78bb022ce41b
sachit-menon <sachit.menon@duke.edu>
2020-04-24 04:34:28 -0400

    Fixed 'Aaron van den Oord' bug - names with more than two words should work now.

--- authorlist_to_rsslist.py
+++ authorlist_to_rsslist.py
@@ -30,9 +30,11 @@ OPML_ENDING = \
 
 def get_arxiv_query(author_name: str)->str:
     author_name_list = author_name.split(' ')
+    author_name_query = '+'.join(author_name_list)
     abbrev_name = author_name.split(' ')
-    abbrev_name[0] = abbrev_name[0][0]
-    return f'http://export.arxiv.org/api/query?search_query=au:%22{author_name_list[0]}+{author_name_list[1]}%22+OR+au:%22{abbrev_name[0]}+{abbrev_name[1]}%22&sortBy=submittedDate&sortOrder=descending&max_results=100'.replace('&', '&amp;')
+    abbrev_name = [abbrev[0] if index != len(abbrev_name)-1 else abbrev for index, abbrev in enumerate(abbrev_name)]
+    abbrev_name_query = '+'.join(abbrev_name)
+    return f'http://export.arxiv.org/api/query?search_query=au:%22{author_name_query}%22+OR+au:%22{abbrev_name_query}%22&sortBy=submittedDate&sortOrder=descending&max_results=100'.replace('&', '&amp;')
 
 def get_opml_lines(data: pd.Series)->pd.Series:
     opml_line = f"""<outline type="rss" text="{data.authorname}" title="{data.authorname}" xmlUrl="{data.queries}"/>"""
